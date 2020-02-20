1aa6c7be7d6a83516de64f7fe22226efa63a8aac
mmelihdogan <doganmmelih@gmail.com>
2020-02-20 20:17:22 +0300

    Driver (done) and Route classes created

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+class Driver {
+    constructor(name, date) {
+        this.name = name;
+        this.date = date;
+    }
 
+    startDate() {
+        // let dates = this.date.split(" ");
+        return new Date(this.date);
+    }
+
+    yearsExperienceFromBeginningOf(year) {
+        return year - this.startDate().getFullYear();
+    }
+}
